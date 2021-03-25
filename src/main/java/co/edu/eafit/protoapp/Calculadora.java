524a055aa31d642c2e6eb09a403cf865d54fd570
eduard damian garcia <yamiyugi43@gmail.com>
2021-03-24 21:56:21 -0500

    intento http

--- src/main/java/co/edu/eafit/protoapp/Calculadora.java
+++ src/main/java/co/edu/eafit/protoapp/Calculadora.java
@@ -14,38 +14,26 @@ public class Calculadora {
     // multiplica el numero
     public Double cuadrado(Double numero) {
         try {
-
             URL url = new URL("https://w8rs3evrl4.execute-api.us-east-1.amazonaws.com/Prod/hello");
             HttpURLConnection conn = (HttpURLConnection) url.openConnection();
             conn.setRequestMethod("GET");
             conn.setRequestProperty("Accept", "application/json");
-    
             if (conn.getResponseCode() != 200) {
                 throw new RuntimeException("Failed : HTTP error code : "
                         + conn.getResponseCode());
             }
-    
             BufferedReader br = new BufferedReader(new InputStreamReader(
                 (conn.getInputStream())));
-    
             String output;
             System.out.println("Output from Server .... \n");
             while ((output = br.readLine()) != null) {
                 System.out.println(output);
             }
-    
             conn.disconnect();
-    
-          } catch (MalformedURLException e) {
-    
+        } catch (MalformedURLException e) {
             e.printStackTrace();
-    
-          } catch (IOException e) {
-    
+        } catch (IOException e) {
             e.printStackTrace();
-    
-          }
-    
         }
         return numero * numero;
     }
