ab4b17330db5e924ee0972c3037f5611509c5da1
eduard damian garcia <yamiyugi43@gmail.com>
2021-03-24 21:52:56 -0500

    intento http

--- src/main/java/co/edu/eafit/protoapp/Calculadora.java
+++ src/main/java/co/edu/eafit/protoapp/Calculadora.java
@@ -1,29 +1,53 @@
 package co.edu.eafit.protoapp;
 
 
-
-import java.net.http.HttpClient;
-import java.net.http.HttpRequest;
-import java.net.http.HttpResponse;
-import java.net.http.HttpResponse.BodyHandler;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 public class Calculadora {
 
     
     // multiplica el numero
     public Double cuadrado(Double numero) {
-        HttpClient client = HttpClient.newHttpClient();
-        HttpRequest request = HttpRequest.newBuilder(URI.create("https://w8rs3evrl4.execute-api.us-east-1.amazonaws.com/Prod/hello/"))
-              .GET()
-              .build();
         try {
-            HttpResponse<String> respuesta=client.send(request, BodyHandler.asString());
-            respuesta.body();
-            return numero * numero;
-        } catch (IOException | InterruptedException e) {
-            // TODO Auto-generated catch block
-            return numero * numero;
-        }  
+
+            URL url = new URL("https://w8rs3evrl4.execute-api.us-east-1.amazonaws.com/Prod/hello");
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setRequestMethod("GET");
+            conn.setRequestProperty("Accept", "application/json");
+    
+            if (conn.getResponseCode() != 200) {
+                throw new RuntimeException("Failed : HTTP error code : "
+                        + conn.getResponseCode());
+            }
+    
+            BufferedReader br = new BufferedReader(new InputStreamReader(
+                (conn.getInputStream())));
+    
+            String output;
+            System.out.println("Output from Server .... \n");
+            while ((output = br.readLine()) != null) {
+                System.out.println(output);
+            }
+    
+            conn.disconnect();
+    
+          } catch (MalformedURLException e) {
+    
+            e.printStackTrace();
+    
+          } catch (IOException e) {
+    
+            e.printStackTrace();
+    
+          }
+    
+        }
+        return numero * numero;
     }
 
     
