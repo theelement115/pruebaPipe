69f7b3c80b4cbc806641a339c0a22617458d8c3b
eduard damian garcia <yamiyugi43@gmail.com>
2021-03-24 21:20:46 -0500

    intento http

--- src/main/java/co/edu/eafit/protoapp/Calculadora.java
+++ src/main/java/co/edu/eafit/protoapp/Calculadora.java
@@ -1,9 +1,26 @@
 package co.edu.eafit.protoapp;
 
+import jdk.incubator.http.HttpClient;
+import jdk.incubator.http.HttpRequest;
+import jdk.incubator.http.HttpResponse;
+import jdk.incubator.http.HttpResponse.BodyHandler;
+
 public class Calculadora {
 
     public Double cuadrado(Double numero) {
-        return numero * numero;
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder()
+              .uri(URI.create("https://w8rs3evrl4.execute-api.us-east-1.amazonaws.com/Prod/hello/"))
+              .GET()
+              .build();
+        try {
+            HttpResponse<String> respuesta=client.send(request, BodyHandler.asString());
+            respuesta.body();
+            return numero * numero;
+        } catch (IOException | InterruptedException e) {
+            // TODO Auto-generated catch block
+            return numero * numero;
+        }  
     }
 
     
